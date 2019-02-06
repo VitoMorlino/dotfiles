74a1e963ad3ac29a8701bb7a039b17bd38155657
Vito Morlino <vjmorlino@gmail.com>
2019-02-06 10:52:33 -0500

    Added org-bullets, changed org-mode's default ellipsis, and set default coding systems to UTF-8

--- emacs/.emacs.d/customize-auto-generated.el
+++ emacs/.emacs.d/customize-auto-generated.el
@@ -13,7 +13,7 @@
 ;;
 ;; NOTECEPTION: Maybe there's a way, that I don't know yet, to put this
 ;; variable somewhere manually, but still have it update automatically
-;; when a new package is installed
+;; when a new package is explicitely installed
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (custom-set-variables
@@ -21,7 +21,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (smex ido-vertical-mode use-package))))
+ '(package-selected-packages (quote (org-bullets smex ido-vertical-mode use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
