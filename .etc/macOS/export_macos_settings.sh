5d013cafecf4ee49399f82937a0004922bea8a02
Vito Morlino <vjmorlino@gmail.com>
2024-08-04 16:10:44 -0400

    added script to export mac settings

---
+++ .etc/macOS/export_macos_settings.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+defaults export com.apple.LaunchServices/com.apple.launchservices.secure.plist launchservices.secure.plist
+defaults export com.apple.symbolichotkeys symbolichotkeys.plist
