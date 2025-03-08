206a9cb3d92eca61faf968d94dfcbecd20f06afb
VitoMorlino <vjmorlino@gmail.com>
2025-03-07 23:21:57 -0500

    add capabilities to gdscript lsp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -948,6 +948,7 @@ require("lazy").setup({
 			require("lspconfig")["gdscript"].setup({
 				name = "godot",
 				cmd = { "ncat", "127.0.0.1", "6005" },
+				capabilities = capabilities,
 			})
 
 			-- Enable the following language servers
