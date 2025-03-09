9cd3ae4e3892b9d718f552d6892404001ec9d356
VitoMorlino <vjmorlino@gmail.com>
2025-03-09 14:14:49 -0400

    cleaned up filename function and comments

--- nvim/.config/nvim/snippets/vimwiki.lua
+++ nvim/.config/nvim/snippets/vimwiki.lua
@@ -1,20 +1,13 @@
 require("luasnip.session.snippet_collection").clear_snippets("vimwiki")
 
--- we have to put this in its own function otherwise the filename is read as this file (`vimwiki.lua`), and not the file the snippet is being generated in
-local filename = function()
-	return f(function(_args, snip)
-		local name = vim.fn.fnamemodify(vim.fn.expand("%"), ":t:r")
-		return name or "oops"
-	end)
-end
-
 local lifeos_relative_path = function()
 	return f(function(_args, snip)
 		local path = vim.fn.fnamemodify(vim.fn.expand("%"), ":h")
+		-- if we're at the root of lifeOS, path is just an extra `.` so we return an empty string
 		if path == "." then
 			return ""
 		end
-		return "/" .. path or "oops"
+		return ("/" .. path) or "oops"
 	end)
 end
 
@@ -33,12 +26,19 @@ return {
 	- 
 	- 
 	]],
-			{ filename = filename(), entry = i(0) }
+			{
+				-- we have to put this in a funtion node so the file name is evaluated where the snippet is being expanded
+				-- otherwise the filename is read as this file (`vimwiki.lua`), and not the file the snippet is being generated in
+				filename = f(function(_args, snip)
+					local name = vim.fn.fnamemodify(vim.fn.expand("%"), ":t:r")
+					return name or "oops"
+				end),
+				entry = i(0),
+			}
 		)
 	),
 
-	-- add a new page with link using our directory structure
-	s(
+	s( -- add a new page with link using our directory structure
 		"+lifeOS new page",
 		fmt(
 			"[{pagename}]({path}/{filename})",
@@ -46,8 +46,7 @@ return {
 		)
 	),
 
-	-- add a new directory with link using our directory structure
-	s(
+	s( -- add a new directory with link using our directory structure
 		"+lifeOS new directory",
 		fmt(
 			"[{pagename}]({path}/{directory}/.{filename})",
