64231560bfd50a2105db730fb085bbf2937726d5
VitoMorlino <17305030+VitoMorlino@users.noreply.github.com>
2025-06-18 01:56:48 -0400

    add clean_startup_registry

--- .etc/windows/clean_startup_registry.ps1
+++ .etc/windows/clean_startup_registry.ps1
@@ -1,19 +1,57 @@
-# for each item in the Run registry list, ask if we should keep the entry
-# if not, delete the registry entry
+###
+# for each item in the Run registry list, ask if we should delete the entry
+###
 
-$startup_apps = Get-ItemProperty -Path Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run
-
-foreach ($app in $startup_apps.PSObject.Properties) {
-        $ignore_list = @("PSPath", "PSParentPath", "PSChildName", "PSProvider")
-        if ($ignore_list -contains $($app.name)) {
-                continue
-        }
-        Write-Host ("entry name: $($app.name)")
-
-        #TODO: copy read-host logic from windows_setup_script to ask about each entry
-        #
-        # add each item to-be-deleted to a list and print the list and ask for confirmation
-        # before deleting anything (don't delete during the loop)
-        #
-        # if user says its not right, just start the loop over and ask about everything again
+$ignore_list = @("PSPath", "PSParentPath", "PSChildName", "PSProvider")
+$whitelist_path = "./startup_registry_whitelist.txt"
+if (Test-Path -Path $whitelist_path) {
+        $whitelist = Get-Content -Path $whitelist_path
+        $ignore_list += $whitelist
 }
+
+$registry_startup_path = "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run"
+$startup_apps = Get-ItemProperty -Path $registry_startup_path
+$startup_apps = $startup_apps.PSObject.Properties | Where-Object { $_.name -notin $ignore_list }
+$entries_to_remove = @()
+
+
+do {
+        $entries_to_remove = @()
+        foreach ($app in $startup_apps) {
+                Write-Host "Prevent " -nonewline
+                Write-Host ($app.name) -nonewline -ForegroundColor yellow
+                Write-Host " from running on startup? (y/n/[w]hitelist/[a]bort): " -nonewline
+                $should_delete = Read-Host
+                switch ($should_delete) {
+                        { $_ -eq 'y' } { $entries_to_remove += $app }
+                        { $_ -in 'w', 'whitelist' } { 
+                                Add-Content -Path "./startup_registry_whitelist.txt" -Value $app.name
+                                Write-Host $app.name -nonewline -foregroundcolor yellow
+                                Write-Host " added to whitelist file at " -nonewline
+                                Write-Host (Resolve-Path -Path $whitelist_path) "`n" -foregroundcolor yellow
+                        }
+                        { $_ -in 'a', 'abort', 'exit' } { 
+                                Write-Host "Aborting... Did not delete anything."
+                                exit
+                        }
+                }
+        }
+
+        if ($entries_to_remove.count -eq 0) {
+                Write-Host "No on-startup entries to remove. Thanks for playing!"
+                break
+        }
+
+        Write-Host "`nEntries to remove:" $entries_to_remove.name -Separator "`n`t" -ForegroundColor yellow
+        $delete_confirmed = $(Write-Host "Confirm deleting the above list of run-on-startup entries? (y/n): " -nonewline; Read-Host)
+} while ($delete_confirmed -ne "y")
+
+foreach ($entry in $entries_to_remove) {
+        Remove-ItemProperty -Path $registry_startup_path -Name $entry.name
+}
+
+# TODO: fix bug where duplicate entries get added to whitelist when user adds to whitelist,
+# but then doesn't confirm the delete list so the loop starts over.
+#
+# fix by adding to an $entries_to_whitelist variable to use after the loop
+
